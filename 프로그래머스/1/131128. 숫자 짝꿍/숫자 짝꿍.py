e8dd48cd597b7581341dc551d4fd672ed9ee852f
dayeon86 <101958798+2021147588@users.noreply.github.com>
2025-03-18 13:55:43 +0900

    [level 1] Title: 숫자 짝꿍, Time: 207.67 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,36 +1,16 @@
 def solution(X, Y):
     answer = ''
-    x = set(X)
-    y = set(Y)
-    inter = x & y
-    if len(inter) == 0:
+    n = "0123456789"
+    
+    for i in n:
+        answer += i*min(X.count(i), Y.count(i))
+    
+    if len(answer) == 0:
         return "-1"
-    
-    c_x = {i: 0 for i in X if i in inter}
-    c_y = {i: 0 for i in Y if i in inter}
-            
-    for i in X:
-        if i in inter:
-            c_x[i]+=1
-    
-    for i in Y:
-        if i in inter:
-            c_y[i]+=1
-            
-    inter = list(inter)
-    if len(inter)==1 and inter[0]=='0':
+    elif len(answer)==answer.count("0"):
         return "0"
-        
-    
+    else:
+        answer = ''.join(sorted(answer, reverse=True))
 
-    for i in inter:
-        if c_x[i] < c_y[i]:
-            answer = answer + c_x[i]*i
-        else:
-            answer = answer + c_y[i]*i
     
-    answer = ''.join(sorted(answer, reverse=True))
-    
-        
-        
     return answer
